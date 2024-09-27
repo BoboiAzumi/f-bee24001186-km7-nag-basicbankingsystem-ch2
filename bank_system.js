d8e90ab0067d416e21e04162d282239c6e3962fa
BoboiAzumi <untukherokudeploy@gmail.com>
2024-09-27 23:08:38 +0700

    feat: User Account

--- bank_system.js
+++ bank_system.js
@@ -1,5 +1,36 @@
 const readlineSync = require("readline-sync")
 const BankAccount = require("./bank_account.js")
+const Human = require("./human")
+
+const Banking = Base => class extends Base{
+    constructor(){
+        this.bankAccount = new BankAccount()
+    }
+}
+
+class UserBank extends Banking(Human){
+    constructor(nama, rekening, pin){
+        this.nama = nama
+        this.rekening = rekening
+        this.pin = pin
+    }
+
+    getNama(){
+        return this.nama
+    }
+
+    getRekening(){
+        return this.nama
+    }
+
+    getPin(){
+        return this.nama
+    }
+
+    getBankingAccount(){
+        return this.nama
+    }
+}
 
 class BankSystem{
     constructor(name){
