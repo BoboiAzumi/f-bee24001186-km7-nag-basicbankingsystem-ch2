c1d92e17298fae4511b3af3b5a76b8a4f2e05455
BoboiAzumi <untukherokudeploy@gmail.com>
2024-09-27 21:02:17 +0700

    feat: add Bank System

---
+++ bank_system.js
@@ -0,0 +1,28 @@
+const readlineSync = require("readline-sync")
+const BankAccount = require("./bank_account.js")
+
+class BankSystem{
+    constructor(name){
+        // TODO
+    }
+
+    #auth(credential){
+        // TODO
+    }
+
+    async doDeposit(credential, ammount){
+        // TODO
+    }
+
+    async doWithdraw(credential, ammount){
+        // TODO
+    }
+
+    async doCekSaldo(credential){
+        // TODO
+    }
+
+    async doSign(rekening, pin){
+        // TODO
+    }
+}
