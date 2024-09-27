196097d7683e7e5fd257cb86220db7504099b75c
BoboiAzumi <untukherokudeploy@gmail.com>
2024-09-27 23:19:48 +0700

    feat: auth

--- bank_system.js
+++ bank_system.js
@@ -2,12 +2,14 @@ const readlineSync = require("readline-sync")
 const BankAccount = require("./bank_account.js")
 const Human = require("./human")
 
+// Helper polymorphism
 const Banking = Base => class extends Base{
     constructor(){
         this.bankAccount = new BankAccount()
     }
 }
 
+// Implementasi inheritance dan polymorphism
 class UserBank extends Banking(Human){
     constructor(nama, rekening, pin){
         this.nama = nama
@@ -34,11 +36,42 @@ class UserBank extends Banking(Human){
 
 class BankSystem{
     constructor(name){
-        // TODO
+        // Simulasi database akun
+        this.account = []
+
+        // Simulasi Session
+        this.credential = []
+
+        // Nama Bank
+        this.bank_name = name
+
+        // Buat simulasi database account
+        this._createDummyAccount()
     }
 
+    // Simulasi insert database, sekaligus simulasi database account (implementasi protected encapsulation)
+    _createDummyAccount(){
+        this.account.push(new UserBank("Naufal Azmi G", "12345", "1234"))
+        this.account.push(new UserBank("Shinomiya Azumi", "54321", "1234"))
+    }
+
+    // Implementasi private
     #auth(credential){
-        // TODO
+        // Implementasi promise
+        return new Promise((resolve, reject) => {
+            try{
+                const check = this.credential.find(v => v.cred == credential)
+    
+                if(check){
+                    resolve(check)
+                }
+    
+                resolve(false)
+            }
+            catch{
+                reject("Error: Kesalahan sistem")
+            }
+        })
     }
 
     async doDeposit(credential, ammount){
